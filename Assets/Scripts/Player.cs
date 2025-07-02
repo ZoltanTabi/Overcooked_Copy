a51cea0b0396e7fa812ec39d4910dafdbe4ee650
Zoltán Tábi <tabizoli98@gmail.com>
2025-07-02 23:24:46 +0200

    Added collision detection with Raycast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,8 @@ public class Player : MonoBehaviour
 
     private bool isWalking = false;
 
+    private float MoveDistance => moveSpeed * Time.deltaTime;
+
     private void Update()
     {
         Vector3 moveDirection = GetMoveDirection();
@@ -38,13 +40,39 @@ public class Player : MonoBehaviour
     private Vector3 GetMoveDirection()
     {
         var inputVector = gameInput.GetMovementVectorNormalized();
+        var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
 
-        return new Vector3(inputVector.x, 0, inputVector.y);
+        float playerRadius = 0.7f; // Adjust based on your player model size
+        float playerHeight = 2f; // Adjust based on your player model height
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, MoveDistance);
+
+        if (!canMove)
+        {
+            var moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, MoveDistance);
+
+            if (canMove)
+            {
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                var moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, MoveDistance);
+
+                if (canMove)
+                {
+                    moveDirection = moveDirectionZ;
+                }
+            }
+        }
+
+        return canMove ? moveDirection : Vector3.zero;
     }
 
     private void Move(Vector3 moveDirection)
     {
-        transform.position += moveSpeed * Time.deltaTime * moveDirection;
+        transform.position += MoveDistance * moveDirection;
     }
 
     private void Rotate(Vector3 moveDirection)
