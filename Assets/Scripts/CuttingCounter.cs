367ca0317bf678c20a99b7e1374a0e68c4f40996
Zoltán Tábi <tabizoli98@gmail.com>
2025-07-11 23:38:28 +0200

    Implemented cutting repipes

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,25 +1,54 @@
+using System.Linq;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOs;
 
     public override bool Interact(Player player)
     {
+        //if (!HasKitchenObject() && player.HasKitchenObject())
+        //{
+        //    if (!TryGetOutputForInput(player.GetKitchenObject().GetKitchenObjectSO(), out var _))
+        //    {
+        //        return false;
+        //    }
+
+        //    player.GetKitchenObject().SetKitchenObjectParent(this);
+
+        //    return true;
+        //}
+
         return base.Interact(player);
     }
 
     public override bool InteractAlternate(Player player)
     {
-        if (HasKitchenObject())
+        if (HasKitchenObject() && TryGetOutputForInput(GetKitchenObject().GetKitchenObjectSO(), out var outputKitchenObjectSO))
         {
             GetKitchenObject().DestroySelf();
 
-            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
 
             return true;
         }
 
         return false;
+    }
+
+    private bool TryGetOutputForInput(KitchenObjectSO inputKitchenObjectSO, out KitchenObjectSO outputKitchenObjectSO)
+    {
+        CuttingRecipeSO cuttingRecipeSO = cuttingRecipeSOs.FirstOrDefault(x => x.input == inputKitchenObjectSO);
+
+        if (cuttingRecipeSO != null)
+        {
+            outputKitchenObjectSO = cuttingRecipeSO.output;
+        }
+        else
+        {
+            outputKitchenObjectSO = null;
+        }
+
+        return outputKitchenObjectSO != null;
     }
 }
