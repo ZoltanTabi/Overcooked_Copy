3084fae361b2f238373ad39f73f914660116c2af
Zoltán Tábi <tabizoli98@gmail.com>
2025-07-12 20:55:37 +0200

    Implemented kitchen object falling
    Kitchen object falling to the trash bottom and not just disappears

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -1,16 +1,47 @@
+using System.Collections;
 using UnityEngine;
 
 public class TrashCounter : BaseCounter
 {
+    [SerializeField] private Transform counterBottomPoint;
+
+    private float fallSpeed = 0f;
+
     public override bool Interact(Player player)
     {
         if (player.HasKitchenObject())
         {
-            player.GetKitchenObject().DestroySelf();
+            if (HasKitchenObject())
+            {
+                StopCoroutine(KitchenObjectFallingAndDestroy());
+                GetKitchenObject().DestroySelf();
+            }
+
+            fallSpeed = 0f;
+
+            player.GetKitchenObject().SetKitchenObjectParent(this);
+            StartCoroutine(KitchenObjectFallingAndDestroy());
 
             return true;
         }
 
         return false;
+    }
+
+    private IEnumerator KitchenObjectFallingAndDestroy()
+    {
+        while (HasKitchenObject() && GetKitchenObject().transform.position.y >= counterBottomPoint.position.y)
+        {
+            fallSpeed += -Physics.gravity.y * Time.deltaTime;
+
+            GetKitchenObject().transform.position += fallSpeed * Time.deltaTime * Vector3.down;
+
+            yield return null;
+        }
+
+        if (HasKitchenObject())
+        {
+            GetKitchenObject().DestroySelf();
+        }
     }
 }
