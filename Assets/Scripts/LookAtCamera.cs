1168fde308152a89b61484972e73437ad01359a3
Zoltán Tábi <tabizoli98@gmail.com>
2025-07-12 18:39:18 +0200

    Implemented Look at The Camera script

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+
+    [SerializeField] private Mode mode = Mode.LookAt;
+
+    private void LateUpdate()
+    {
+        if (Camera.main == null)
+        {
+            return;
+        }
+
+        switch (mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+
+            case Mode.LookAtInverted:
+                Vector3 directionFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + directionFromCamera);
+                break;
+
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
+    }
+}
