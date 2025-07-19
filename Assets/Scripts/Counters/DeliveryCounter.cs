64902ec622537346a0d1aef4597ef196efffe4f2
Zoltán Tábi <tabizoli98@gmail.com>
2025-07-19 13:48:23 +0200

    Added plate moving on delivery counter

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -4,6 +4,9 @@ public class DeliveryCounter : BaseCounter
 {
     public static DeliveryCounter Instance { get; private set; }
 
+    [SerializeField] private Transform counterDeliveryPoint;
+    [SerializeField] private float deliverySpeed = 2f;
+
     private void Awake()
     {
         if (Instance == null)
@@ -16,13 +19,38 @@ public class DeliveryCounter : BaseCounter
         }
     }
 
+    private void Update()
+    {
+        if (!GameManager.Instance.IsGamePlaying())
+        {
+            return;
+        }
+
+        if (HasKitchenObject())
+        {
+            GetKitchenObject().transform.position = Vector3.MoveTowards(GetKitchenObject().transform.position, counterDeliveryPoint.position, deliverySpeed * Time.deltaTime);
+
+            if (Vector3.Distance(GetKitchenObject().transform.position, counterDeliveryPoint.position) < 0.01f)
+            {
+                DeliveryManager.Instance.DeliverRecipe(GetKitchenObject() as PlateKitchenObject);
+
+                GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+
     public override bool Interact(Player player)
     {
-        if (player.HasKitchenObject() && player.GetKitchenObject().TryGetPlate(out var plateKitchenObject))
+        if (player.HasKitchenObject() && player.GetKitchenObject().TryGetPlate(out var _))
         {
-            DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
+            if (HasKitchenObject())
+            {
+                DeliveryManager.Instance.DeliverRecipe(GetKitchenObject() as PlateKitchenObject);
 
-            plateKitchenObject.DestroySelf();
+                GetKitchenObject().DestroySelf();
+            }
+
+            player.GetKitchenObject().SetKitchenObjectParent(this);
 
             return true;
         }
